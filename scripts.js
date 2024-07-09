72ce468d919481114b2d62862e55f05e42c03235
cyan1992 <maarefimorteza@gmail.com>
2024-07-09 17:47:04 +0330

    Js Added Styles image buttons updated

---
+++ scripts.js
@@ -0,0 +1,6 @@
+function cyan() {
+    // var cyan = document.getElementsByClassName('btn_cyan');
+    let target = document.getElementById("colorset__iximage");
+    target.src = "assets/png/cyanbmw.png";
+    target.setAttribute('src', "assets/png/cyanbmw.png");
+}
