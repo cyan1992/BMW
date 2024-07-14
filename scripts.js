ec874e4d73026ba5cfa0a913dbe6e5e63c11777f
cyan1992 <maarefimorteza@gmail.com>
2024-07-14 20:12:30 +0330

    SRC Changed Pass in JS

--- scripts.js
+++ scripts.js
@@ -1,6 +1,41 @@
-function cyan() {
-    // var cyan = document.getElementsByClassName('btn_cyan');
-    let target = document.getElementById("colorset__iximage");
-    target.src = "assets/png/cyanbmw.png";
-    target.setAttribute('src', "assets/png/cyanbmw.png");
-}
+// function cyan() {
+//     // var cyan = document.getElementsByClassName('btn_cyan');
+//     let target = document.getElementById("colorset__iximage");
+//     target.src = "assets/png/cyanbmw.png";
+//     // target.setAttribute('src', "assets/png/cyanbmw.png");
+// }
+
+
+// function cyan() {
+//     var img = document.getElementById('colorset__iximage');
+//     if (img.src.endsWith('/assets/png/lightoff.png')) {
+//         img.src = 'assets/png/cyanbmw.png';
+//     } else {
+//         img.src = 'assets/png/cyanbmw.png';
+//     }
+// }
+
+
+
+    //var image = document.getElementsByName('btn_cyan');
+
+    // image.addEventListener('click', function(){
+    //     cyan();
+    // });
+    let hhhhh= document.getElementById("btn_cyan");
+    // hhhhh.addEventListener('onmouseover',function(){
+    //     console.log("bahman")
+    //     let target = document.getElementById("colorset__iximage");
+    //     target.setAttribute('src', "assets/png/cyanbmw.png");
+    // })
+    function g(){
+        console.log("bahman")
+        let target = document.getElementById("colorset__iximage");
+        target.setAttribute('src', "assets/png/cyanbmw.png");
+    }
+    function cyan(){
+        var target = document.getElementById("colorset__iximage");
+        console.log(target)
+         target.setAttribute('src', "assets/png/cyanbmw.png");
+        //target.src = 'assets/png/cyanbmw.png';
+    }
